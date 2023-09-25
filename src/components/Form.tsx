f7c504f0c1b33f8df7562807ec4aa9f98cef4b81
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-25 14:31:13 -0600

    06

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,6 +9,13 @@ export default function Form() {
     calories: 0
   })
 
+  const handleChange = (e) => {
+    setActivity({
+      ...activity,
+      [e.target.id]: e.target.value
+    })
+  }
+
   return (
     <form 
       className="space-y-5 bg-white shadow p-10 rounded-lg"
@@ -19,6 +26,7 @@ export default function Form() {
             className="border border-slate-300 p-2 rounded-lg w-full bg-white"
             id="category"
             value={activity.category}
+            onChange={handleChange}
           >
             {categories.map(category => (
               <option
@@ -39,6 +47,7 @@ export default function Form() {
             className="border border-slate-300 p-2 rounded-lg"
             placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
             value={activity.name}
+            onChange={handleChange}
           />
       </div>
 
@@ -50,6 +59,7 @@ export default function Form() {
             className="border border-slate-300 p-2 rounded-lg"
             placeholder="Calorias. ej. 300 o 500"
             value={activity.calories}
+            onChange={handleChange}
           />
       </div>
 
