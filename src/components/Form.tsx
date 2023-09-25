da1a0661fab77694150ca529bd2a9d0751fe09bb
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-25 16:39:25 -0600

    07

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, ChangeEvent } from "react"
 import { categories } from "../data/categories"
 
 export default function Form() {
@@ -9,7 +9,7 @@ export default function Form() {
     calories: 0
   })
 
-  const handleChange = (e) => {
+  const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
     setActivity({
       ...activity,
       [e.target.id]: e.target.value
