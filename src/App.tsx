44dfeae049fccbead7543c3b5f97085d9385cb6f
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-25 14:01:41 -0600

    04

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ function App() {
         </header>
 
         <section className="bg-lime-500 py-20 px-5">
-          <div className="max-w-4xl mx-auto">
-              <Form />
-          </div>
+            <div className="max-w-4xl mx-auto">
+                <Form />
+            </div>
         </section>
     </>
   )
