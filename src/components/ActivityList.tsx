25a82a9119d3e4a7ddfaa6df47f82027f2cff0e3
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-27 11:49:37 -0600

    20

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,4 +1,6 @@
 import { Activity } from "../types"
+import { categories } from "../data/categories"
+import { useMemo } from "react"
 
 type ActivityListProps = {
     activities: Activity[]
@@ -6,6 +8,10 @@ type ActivityListProps = {
 
 
 export default function ActivityList({activities} : ActivityListProps) {
+
+    const categoryName = useMemo(() => 
+        (category: Activity['category']) => categories.map( cat => cat.id === category ? cat.name : '' )
+    , [activities])
 
     return (
         <>
@@ -16,8 +22,9 @@ export default function ActivityList({activities} : ActivityListProps) {
             {activities.map( activity => (
                 <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
                     <div className="space-y-2 relative"> 
-                        <p>
-                            {activity.category}
+                        <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
+                        ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                            {categoryName(+activity.category)}
                         </p>
                         <p className="text-2xl font-bold pt-5">{activity.name}</p>
                         <p className="font-black text-4xl text-lime-500">
