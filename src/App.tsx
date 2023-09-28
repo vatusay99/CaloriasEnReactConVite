9a142f98f88b0d7d36513687f1390304608bc609
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-28 11:25:44 -0600

    30

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useEffect } from 'react'
+import { useReducer, useEffect, useMemo } from 'react'
 import Form from "./components/Form"
 import { activityReducer, initialState } from './reducers/activity-reducer'
 import ActivityList from './components/ActivityList'
@@ -10,6 +10,8 @@ function App() {
     useEffect(() => {
         localStorage.setItem('activities', JSON.stringify(state.activities))
     }, [state.activities])
+
+    const canRestartApp = () => useMemo(() => state.activities.length, [state.activities])
     
     return (
         <>
@@ -18,6 +20,14 @@ function App() {
                     <h1 className="text-center text-lg font-bold text-white uppercase">
                         Contador de Calorias
                     </h1>
+
+                    <button
+                        className='bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10'
+                        disabled={!canRestartApp()}
+                        onClick={() => dispatch({type: 'restart-app'})}
+                    >
+                        Reiniciar App
+                    </button>
                 </div>
             </header>
 
