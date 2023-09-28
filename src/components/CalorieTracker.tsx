ca3e8a6934316ca43b50f5a0aff6bbc048836425
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-09-28 12:30:11 -0600

    35

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -11,8 +11,8 @@ export default function CalorieTracker({activities} : CalorieTrackerProps) {
     // Contadores
     const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities])
     const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [activities])
+    const netCalories = useMemo(() => caloriesConsumed - caloriesBurned, [activities])
     
-
     return (
         <>
             <h2 className="text-4xl font-black text-white text-center">Resumen de Calorias</h2>
@@ -25,6 +25,10 @@ export default function CalorieTracker({activities} : CalorieTrackerProps) {
                 <CalorieDisplay
                     calories={caloriesBurned}
                     text="Ejercicio"
+                />
+                <CalorieDisplay
+                    calories={netCalories}
+                    text="Diferencia"
                 />
             </div>
  
